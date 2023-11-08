bdac2c32b9f354f23757ef7292badfd3d1e4145c
david birt <dbirt3168@outlook.com>
2023-11-08 14:56:16 -0500

    first commit

---
+++ PageObjects/IPageObject.cs
@@ -0,0 +1,116 @@
+﻿using OpenQA.Selenium.Remote;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PageObjects.Utils;
+
+namespace PageObjects.PageObjects
+{
+    public interface IDialogObject
+    {
+        IWebElement DialogElement { get; set; }
+        IWebDriver Driver { get; set; }
+    }
+
+    /// <summary>
+	/// That Pageeeee object
+	/// </summary>
+	public interface IPageObject
+    {
+        string Address { get; }
+        IWebDriver Driver { get; set; }
+        bool AtCorrectLocation(string className = null);
+    }
+
+    // Fetches elements from the page and builds components for the test
+    public abstract class PageObject
+    {
+        public PageObject()
+        {
+        }
+
+        public PageObject(IWebElement element)
+        {
+        }
+
+        public PageObject(IWebDriver driver)
+        {
+            Driver = driver;
+        }
+
+        public IWebDriver Driver { get; set; }
+
+        //Methods
+        public bool PageInErrorState()
+        {
+            bool loaded = Driver.WaitForCondition<bool>((d) =>
+            {
+                return d.FindElements(By.ClassName("alert-danger")).Any(e => e.Displayed);
+            }, 10);
+            return loaded;
+        }
+
+     
+        public virtual bool AtCorrectLocation(string className = null)
+        {
+            Trace.WriteLine("Waiting for page to load");
+            bool loaded = Driver.WaitForCondition<bool>((d) =>
+            {
+                return d.FindElements(By.ClassName(className)).Any(e => e.Displayed);
+            }, 30);
+            return loaded;
+        }
+
+        public virtual T ExecuteScript<T>(string script)
+        {
+            try
+            {
+                return (T)((RemoteWebDriver)Driver).ExecuteScript(script);
+            }
+            catch (System.Exception ex)
+            {
+                Trace.WriteLine("ERROR: " + ex.Message);
+                return default(T);
+            }
+        }
+
+        /// <summary>
+        /// Waits up to 8 seconds for a question to load and its text to be dislpayed. 
+        /// </summary>
+        /// <param name="searchText"></param>
+        /// <returns></returns>
+        public virtual bool TextDisplayed(string searchText)
+        {
+            var selection = Driver.FindElement(By.TagName("body"));
+            bool loaded = Driver.WaitForCondition<bool>((d) =>
+            {
+                return selection.Text.Contains(searchText);
+            }, 8);
+            return loaded;
+        }
+
+        public virtual bool ErrorText()
+        {
+            //this method specifically checks for Toastr error messages in the application.
+            bool _errortext = Driver.WaitForCondition<bool>((d) =>
+            {
+                return d.FindElements(By.ClassName("toast-error")).Any(e => e.Displayed);
+            }, 5);
+            return _errortext;
+        }
+
+        public bool SuccessText()
+        {
+            //this method specifically checks for Toastr success messages in the application.
+            bool _successtext = Driver.WaitForCondition<bool>((d) =>
+            {
+                return d.FindElements(By.ClassName("toast-success")).Any(e => e.Displayed);
+            }, 5);
+            return _successtext;
+        }
+    }
+}
---
+++ PageObjects/StackDemoProductsPage.cs
@@ -0,0 +1,101 @@
+﻿using OpenQA.Selenium;
+using PageObjects.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PageObjects.PageObjects
+{
+    public class StackDemoProductsPage : PageObject, IPageObject
+    {
+        public string Address => throw new NotImplementedException();
+
+        public List<StackDemoShelfItem> Products
+        {
+            get
+            {
+                var _listo = new List<StackDemoShelfItem>();
+                var rows = Driver.FindElements(By.CssSelector(".shelf-container .shelf-item"));
+                foreach (var row in rows)
+                {
+                    var rowToAdd = new StackDemoShelfItem(row);
+                    _listo.Add(rowToAdd);
+                }
+                return _listo;
+            }
+        }
+
+        StackDemoShoppingCart _cartToRtn;
+        public StackDemoShoppingCart Cart
+        { 
+            get
+            {
+                if(_cartToRtn == null) _cartToRtn = new StackDemoShoppingCart(Driver.FindElement(By.CssSelector("div.float-cart")));
+                return _cartToRtn;
+            }
+        }
+
+        public override bool AtCorrectLocation(string className = null)
+        {
+            return Driver.WaitForCondition<bool>((d) =>
+            {
+                return d.FindElements(By.CssSelector(".shelf-container .shelf-item")).Any();
+            }, 4);
+        }
+    }
+
+    public class StackDemoShelfItem
+    {
+        IWebElement _parentElement;
+        public StackDemoShelfItem(IWebElement parentElement)
+        {
+            _parentElement = parentElement;
+        }
+
+        public string ProductName => _parentElement.FindElement(By.CssSelector("p.shelf-item__title")).Text;
+        public string ProductPrice => _parentElement.FindElement(By.CssSelector("div.shelf-item__price .val")).Text;
+        public IWebElement btn_AddToCart => _parentElement.FindElement(By.CssSelector(".shelf-item__buy-btn"));
+    }
+
+    public class StackDemoShoppingCart
+    {
+        IWebElement _parentElement;
+        public StackDemoShoppingCart(IWebElement parentElement)
+        {
+            _parentElement = parentElement;
+        }
+
+        public IWebElement btn_checkout => _parentElement.FindElement(By.CssSelector("div.buy-btn"));
+        public string CartTotalPrice => _parentElement.FindElement(By.CssSelector(".float-cart .float-cart__footer .sub-price__val")).Text;
+
+        public List<StackDemoShoppingCartItem> Products
+        {
+            get
+            {
+                var _listo = new List<StackDemoShoppingCartItem>();
+                var rows = _parentElement.FindElements(By.CssSelector("div.shelf-item"));
+                foreach (var row in rows)
+                {
+                    var rowToAdd = new StackDemoShoppingCartItem(row);
+                    _listo.Add(rowToAdd);
+                }
+                return _listo;
+            }
+        }
+    }
+
+    public class StackDemoShoppingCartItem
+    {
+        IWebElement _parentElement;
+        public StackDemoShoppingCartItem(IWebElement parentElement)
+        {
+            _parentElement = parentElement;
+        }
+
+        public string ProductName => _parentElement.FindElement(By.CssSelector(".shelf-item__details p.title")).Text;
+        public string ProductPrice => _parentElement.FindElement(By.CssSelector(".shelf-item__price p")).Text;
+
+    }
+}
